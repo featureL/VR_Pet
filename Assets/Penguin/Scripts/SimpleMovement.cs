cf392edab061361d6d19c88eb268f30b17ae3091
VladEn <credo098@gmail.com>
2020-03-14 20:04:27 +0300

    Initial commit

---
+++ Assets/Penguin/Scripts/SimpleMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    Animator anim;
+    UnityEngine.AI.NavMeshAgent agent;
+    LookAt lookAt;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.updatePosition = false;
+        lookAt = GetComponent<LookAt>();
+    }
+
+    float maxDX = 0;
+    float maxDY = 0;
+
+    void Update()
+    {
+        Vector3 deltaPosition = agent.nextPosition - transform.position;
+        float dx = Vector3.Dot(transform.forward, deltaPosition);
+        float dy = Vector3.Dot(transform.right, deltaPosition);
+        maxDX = Mathf.Max(maxDX, dx);
+        maxDY = Mathf.Max(maxDY, dy);
+        Vector2 movement = new Vector2(dx, dy);
+
+        Vector2 velocity = movement / Time.deltaTime;
+        float blend = 0;
+        bool shouldMove = (velocity.magnitude >= 0.01);
+
+        //если требуется поворот на большой угол, то пингвин повернётся на месте, не будет перемещаться
+        if(!shouldMove)
+        {
+            blend = 0;
+        }
+        else if (Mathf.Abs(velocity.y) > 0.1)
+        {
+            agent.nextPosition = transform.position;
+            if (velocity.y < 0)
+                blend = 0;
+            else blend = 1;
+                blend = Mathf.Max(blend, -1);
+        }
+        else
+        {
+            transform.position = agent.nextPosition;
+            blend = 0.5f;
+        }
+
+        //Debug.Log("deltaPosition = " + deltaPosition + "; velocity = " + velocity + "; blend = " + blend);
+        anim.SetBool("move", shouldMove);
+        anim.SetFloat("blend", blend);
+        anim.SetFloat("velx", dx);
+        anim.SetFloat("vely", dy);
+
+        if (lookAt)
+            lookAt.lookAtTargetPosition = agent.steeringTarget + transform.forward;
+    }
+}
---
+++ Assets/_scripts_penguin/penguinAI.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class penguinAI : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    
+    private int _layermask = -1;
+    public float _maxDistance = 25;
+    private Vector3 _origin;
+    private bool isCoroutineExecuting = false;
+
+
+    private Actions _lastAction;
+
+
+    private enum Actions
+    {
+        Move,
+        Stay
+    }
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        _origin = transform.position;
+        Move();
+    }
+
+    
+    void Update()
+    {
+        // Проверяется , не в пути ли пингвин , либо он еще не достоял свое. 
+        if (!isCoroutineExecuting && agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance == 0)
+        {
+            //Выбирается действие для пингвина. 
+            int rand = Random.Range(0, 8);
+            if(rand > 2) { Debug.Log("Move"); Move(); }
+            else { Debug.Log("Stay"); StartCoroutine(Stay());}
+            
+        }
+            
+    }
+
+
+    public void Move()
+    {
+        //Debug.Log(agent.remainingDistance);
+        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * _maxDistance;
+        //Debug.Log(UnityEngine.Random.insideUnitSphere);
+        randomDirection += _origin;
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randomDirection, out navHit, _maxDistance, _layermask);
+        agent.SetDestination(navHit.position);
+        return;
+    }
+
+    IEnumerator Stay()
+    {
+        if (isCoroutineExecuting)
+            yield break;
+
+        isCoroutineExecuting = true;
+        //Время , которое пингвин стоит на месте. 
+        yield return new WaitForSeconds(5);
+        isCoroutineExecuting = false;
+    }
+}
